49be232d4bfc9d2213fd7754fa2bd4a4a171d481
Gil Alpert <alpertgil@gmail.com>
2024-02-15 23:02:08 +0200

    Fetch artist correctly, Download the highest quality mp4 and convert to mp3 in the fixed folder (with playlist name)

--- export_playlist.py
+++ export_playlist.py
@@ -10,20 +10,15 @@ from selenium.webdriver.common.by import By
 
 from tqdm import tqdm
 
+from pytube import YouTube
+from moviepy.editor import *
+import ffmpeg
+
 
 class SpotifyFetcher:
-    def __init__(self, link: str):
+    def __init__(self, link: str, d):
         self.playlist_link = link
-        self.user_data_dir = r"C:\Users\alper\AppData\Local\Google\Chrome\User Data"
-
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--silent")
-        chrome_options.add_argument(f"user-data-dir={self.user_data_dir}")
-        self.driver = webdriver.Chrome(
-            'C:/Program Files/ChromeDriver/chromedriver.exe', options=chrome_options
-        )
-
+        self.driver = d
         self.playlist_response = None
         self.songs = []
         self.playlist_name = None
@@ -39,8 +34,6 @@ class SpotifyFetcher:
         time.sleep(1)
 
         soup = BeautifulSoup(self.driver.page_source, 'html.parser')
-        #   <meta content="82" name="music:song_count"/>
-        #   <meta content="My Rock" name="twitter:title"/>
         self.playlist_name = soup.find("meta", {"name": "twitter:title"})["content"]
         self.song_count = int(soup.find("meta", {"name": "music:song_count"})["content"])
         pbar = tqdm(total=self.song_count, desc="Fetching songs from Spotify")
@@ -56,11 +49,10 @@ class SpotifyFetcher:
                     name_artist_div = track_row.find("div", {"aria-colindex": "2"}).find("div")
                 except AttributeError:
                     break
-                name = name_artist_div.find("div").text
-                artist = name_artist_div.find("a").text
+                name = name_artist_div.find("div", {"dir": "auto"}).text
+                artist = name_artist_div.find("a", {"dir": "auto"}).text
                 songs.add((name, artist))
             new_song_count = len(songs) - prev_song_count
-            # print(f"Loaded {new_song_count} new songs. Total so far: {len(songs)}")
             pbar.update(new_song_count)
             self.driver.execute_script("arguments[0].scrollBy(0, 500);", grid)
             time.sleep(0.5)
@@ -73,12 +65,82 @@ class SpotifyFetcher:
     def get_songs(self) -> list[tuple[str, str]]:
         return self.songs
 
+    def get_playlist_name(self) -> str:
+        return self.playlist_name
+
+
+class YoutubeDownloader:
+    def __init__(self, songs: list[tuple[str, str]], d, name):
+        self.driver = d
+        self.songs = songs
+        self.songs_to_download = []
+        self.destination = os.path.join(rf"C:\Users\alper\Music", name)
+        os.makedirs(self.destination, exist_ok=True)
+
+    def get_songs_to_download(self):
+        for song in tqdm(self.songs, desc="Fetching YouTube links"):
+            link = self.get_song_link(song)
+            self.songs_to_download.append((*song, link))
+
+    def get_song_link(self, song: tuple[str, str]) -> str:
+        base_url = "https://www.youtube.com/results?search_query="
+        name, artist = song
+        search_term = f"{name} - {artist}"
+        url = base_url + search_term.replace(" ", "+")
+        self.driver.get(url)
+        wait = WebDriverWait(self.driver, 15)
+        wait.until(expected_conditions.presence_of_element_located((By.ID, "video-title")))
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        first_result = soup.find("a", {"id": "video-title"})
+        video_base_url = "https://www.youtube.com"
+        return video_base_url + first_result["href"]
+
+    def download_songs(self):
+        for song_link in tqdm(self.songs_to_download, desc="Downloading songs"):
+            name, artist, link = song_link
+            self.download_song(name, artist, link)
+
+    def download_song(self, name: str, artist: str, link: str):
+        filename = f"{name} - {artist}.mp3"
+        path = os.path.join(self.destination, filename)
+        self.download_youtube_song(link, path)
+
+    def download_youtube_song(self, url, output_path):
+        folder = os.path.dirname(output_path)
+        temp_video_path = os.path.join(folder, 'temp_video.mp4')
+        yt = YouTube(url)
+        # Download the video
+        yt.streams.first().download(output_path=folder, filename='temp_video.mp4')
+        # Load the video file
+        video = VideoFileClip(temp_video_path)
+        # Extract audio from the video
+        audio = video.audio
+        # Write the audio to a new file
+        audio.write_audiofile(output_path)
+        # Close the video and audio files
+        video.close()
+        audio.close()
+        # Delete the temporary video file
+        os.remove(temp_video_path)
+
 
 def main(playlist_link: str):
-    fetcher = SpotifyFetcher(playlist_link)
+    user_data_dir = r"C:\Users\alper\AppData\Local\Google\Chrome\User Data"
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--silent")
+    chrome_options.add_argument(f"user-data-dir={user_data_dir}")
+    driver = webdriver.Chrome(
+        'C:/Program Files/ChromeDriver/chromedriver.exe', options=chrome_options
+    )
+
+    fetcher = SpotifyFetcher(playlist_link, driver)
     fetcher.fetch_playlist()
     songs = fetcher.get_songs()
-    print(songs)
+    downloader = YoutubeDownloader(songs, driver, fetcher.get_playlist_name())
+    downloader.get_songs_to_download()
+    downloader.download_songs()
 
 
 if __name__ == '__main__':
